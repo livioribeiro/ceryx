b6fa4ba58ac535efd40c4fc4371f86606e7e12ea
Livio Ribeiro <livio@codata.pb.gov.br>
2017-05-03 10:41:07 -0300

    path based routing on proxy

--- proxy/nginx/lualib/routelib.lua
+++ proxy/nginx/lualib/routelib.lua
@@ -1,67 +1,36 @@
-function string.starts(String, Start)
-   return string.sub(String, 1, string.len(Start)) == Start
+function string.starts(str,s) return string.sub(str,1,string.len(s))==s end
+function string.ends(str,e) return e==''or string.sub(str,-string.len(e))==e end
+function string.ocurrn(str,s)
+  local c=0
+  for s in string.gfind(str,s) do c=c+1 end
+  return c
 end
-
-function string.ends(String, End)
-   return End == '' or string.sub(String, -string.len(End)) == End
+local P={}
+function P.sort(lhs,rhs)
+  local l=string.ocurrn(lhs,'/')
+  local r=string.ocurrn(rhs,'/')
+  if l==r then return lhs>rhs end
+  return l>r
 end
-
-function string.occurrencies(String, Search)
-    local count = 0
-    for s in string.gfind(String, Search) do
-        count = count + 1
-    end
-    return count
+function P.norm(p)
+  if not string.ends(p,'/') then return p..'/'
+  else return p
+  end
 end
-
-local routepath = {}
-
-function routepath.sort(lhs, rhs)
-    local l_occur = string.occurrencies(lhs, '/')
-    local r_ocurr = string.occurrencies(rhs, '/')
-
-    if l_occur == r_ocurr then
-        return lhs > rhs
-    end
-
-    return l_occur > r_ocurr
+function P.matches(p,s)
+  local np=P.norm(p)
+  local ns=P.norm(s)
+  return string.starts(p, ns) or np==ns
 end
-
-function routepath.normalize(path)
-    if not string.ends(path, '/') then
-        return path .. '/'
-    else
-        return path
-    end
-end
-
-function routepath.starts(Path,Start)
-    local norm_path = routepath.normalize(Path)
-    local norm_start = routepath.normalize(Start)
-
-    return string.starts(norm_path, norm_start)
-end
-
-local prefixed_host = KEYS[1]
-local given_path = ARGV[1]
-
-local paths = {}
-local idx = 1
-
-local routes = redis.call('keys', prefixed_host .. ':*')
-
+local _host,_path = KEYS[1],ARGV[1]
+local paths,i = {},1
+local routes = redis.call('keys',_host..':*')
 for _,r in pairs(routes) do
-    local path = string.gsub(r, prefixed_host .. ':', '')
-    if routepath.starts(given_path, path) then
-        paths[idx] = path
-        idx = idx + 1
-    end
+  local path=string.gsub(r,_host..':','')
+  if P.matches(_path,path) then paths[i]=path; i=i+1 end
 end
-
-table.sort(paths, routepath.sort)
-
-if paths[1] == nil then
-    return nil
+table.sort(paths, P.sort)
+if #paths==0 then
+  return redis.call('get', _host)
 end
-
-return redis.call('get', prefixed_host .. ':' .. paths[1])
+return redis.call('get',_host..':'..paths[1])
--- proxy/nginx/lualib/router.lua
+++ proxy/nginx/lualib/router.lua
@@ -1,59 +1,79 @@
 local container_url = ngx.var.container_url
 local host = ngx.var.host
--- nginx $uri contains the request path in this context
-local path = ngx.var.uri
+local path = ngx.var.uri -- nginx $uri contains the request path in this context
+local cache = ngx.shared.ceryx
+
+-- Setup
+
+-- Debug Mode
+local debug_mode = os.getenv("DEBUG")
+
+-- Cache expiration time
+local cache_exptime = os.getenv("CACHE_EXPTIME")
+if not cache_exptime then
+    cache_exptime = 60 -- 1 minute
+else
+    cache_exptime = tonumber(exptime)
+end
+
+-- Redis host
+local redis_host = os.getenv("REDIS_HOST")
+if not redis_host then
+    redis_host = "127.0.0.1"
+end
+
+-- Redis port
+local redis_port = os.getenv("REDIS_PORT")
+if not redis_port then
+    redis_port = 6379
+end
+
+-- Redis prefix
+local redis_prefix = os.getenv("REDIS_PREFIX")
+if not redis_prefix then redis_prefix = "ceryx" end
+
+-- End Setup
 
 -- Check if key exists in local cache
-local cache = ngx.shared.ceryx
 local res, flags = cache:get(host .. path)
 if res then
     ngx.var.container_url = res
     return
 end
 
+-- Prepare Redis script
+local route_script, flags = cache:get('route_script')
+if not route_script then
+    -- Read routelib.lua file
+    io.input('/usr/local/openresty/nginx/lualib/routelib.lua')
+    route_script, err = io.read('*all')
+
+    if err then
+        ngx.log(ngx.ERR, 'failed to read redis routelib script: ' .. err)
+        ngx.exit(ngx.ERROR)
+    end
+
+    if not debug_mode then
+        cache:set('route_script', route_script)
+    end
+end
+
 local redis = require "resty.redis"
 local red = redis:new()
 red:set_timeout(100) -- 100 ms
-local redis_host = os.getenv("CERYX_REDIS_HOST")
-if not redis_host then redis_host = "127.0.0.1" end
-local redis_port = os.getenv("CERYX_REDIS_PORT")
-if not redis_port then redis_port = 6379 end
 local res, err = red:connect(redis_host, redis_port)
 
 -- Exit if could not connect to Redis
 if not res then
-    ngx.log(ngx.ERR, "failed to connect to redis: " .. err)
+    ngx.log(ngx.ERR, "failed to connect to redis at " .. redis_host .. ':' .. redis_port .. ': ' .. err)
     ngx.exit(ngx.ERROR)
 end
 
--- Setup redis script
-local redis_script, flags = cache:get('redis_script')
-if not redis_script then
-    -- Read routelib.lua file
-    io.input('/usr/local/openresty/nginx/lualib/routelib.lua')
-    routelib = io.read('*all')
-
-    -- Flush Redis scripts
-    red:script('flush')
-
-    -- Load script into Redis
-    hash, err = red:script('load', routelib)
-
-    if err then
-        ngx.log(ngx.ERR, "failed to load script into redis: " .. err)
-        ngx.exit(ngx.ERROR)
-    end
-
-    cache:set('redis_script', routelib)
-end
-
 -- Construct Redis key
-local prefix = os.getenv("CERYX_REDIS_PREFIX")
-if not prefix then prefix = "ceryx" end
-local key = prefix .. ":routes:" .. host
+local key = redis_prefix .. ":routes:" .. host
 
 -- Try to get target for host
-res, err = red:eval(redis_script, 1, key, path)
+res, err = red:eval(route_script, 1, key, path)
 
 -- Exit if route could not be read
 if err then
@@ -75,12 +95,6 @@ if not res or res == ngx.null then
 end
 
 -- Save found key to local cache for specified time in seconds
-local exptime = os.getenv("CERYX_CACHE_EXPTIME")
-if not exptime then
-    exptime = 60 -- 1 minute
-else
-    exptime = tonumber(exptime)
-end
-cache:set(host, res, 5)
+cache:set(host, res, cache_exptime)
 
 ngx.var.container_url = res
